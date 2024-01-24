a2eea29bf0f9dbad4f3c1a94ca5048b5007c1868
daniel-liberto <daniel.liberto@hotmail.com>
2024-01-24 12:33:21 -0300

    hibernate first commit

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -2,17 +2,29 @@ package application;
 
 import domain.Person;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.util.Locale;
 
 public class Program {
   public static void main(String[] args) {
     Locale.setDefault(Locale.US);
 
-    Person p1 = new Person(1, "Greg", "greg@gmail.com");
-    Person p2 = new Person(2, "John", "john@gmail.com");
-    Person p3 = new Person(3, "Chris", "chris@gmail.com");
-    System.out.println(p1);
-    System.out.println(p2);
-    System.out.println(p3);
+
+
+    Person p1 = new Person(null, "Greg", "greg@gmail.com");
+    Person p2 = new Person(null, "John", "john@gmail.com");
+    Person p3 = new Person(null, "Chris", "chris@gmail.com");
+
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("example-jpa");
+    EntityManager em = emf.createEntityManager();
+    em.getTransaction().begin();
+    em.persist(p1);
+    em.persist(p2);
+    em.persist(p3);
+    em.getTransaction().commit();
+    System.out.println("Done!");
+
   }
 }
